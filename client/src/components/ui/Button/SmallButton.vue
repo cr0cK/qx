7f1bd031568977db5b8fcc7f8e3728fcabfccfd2
Alexis Mineaud <alexis.mineaud@gmail.com>
2017-01-06 17:59:02 +0100

    Buttons accept title.

--- client/src/components/ui/Button/SmallButton.vue
+++ client/src/components/ui/Button/SmallButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="onClickHandler">
+  <button @click="onClickHandler" :title="title">
     <i :class="getClasses"></i>
   </button>
 </template>
@@ -10,6 +10,7 @@ export default {
 
   props: {
     icon: String,
+    title: String,
     onClickHandler: Function,
   },
 
